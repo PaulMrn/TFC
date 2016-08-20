140c84f409970ac48c3f7226ba49387e96de1602
PaulMrn <paul.morane@hetic.net>
2016-08-20 19:25:34 +0200

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import {Routes} from '@angular/router';
+import {About} from './about/about';
+import {Home} from './home/home';
+
+export const rootRouterConfig: Routes = [
+  {path: '', redirectTo: 'home', terminal: true},
+  {path: 'home', component: Home},
+  {path: 'about', component: About},
+];
+
