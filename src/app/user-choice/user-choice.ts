42ceaa9865a4d87e009fa677e3c341dc6fb3d36b
Quentin <quentin.lemarchand@hetic.net>
2016-08-21 15:36:08 +0200

    dynamic menu

--- src/app/user-choice/user-choice.ts
+++ src/app/user-choice/user-choice.ts
@@ -7,4 +7,11 @@ import {Component} from '@angular/core';
 })
 
 export class UserChoice {
+    public chosenServices = [];
+    public servicesList = ['Flight', "Hotel", "Restaurant", "Transfer & Car", "Delivery", "Interpreter", "VAT Recovery", "Host & Hostess"];
+    
+    selectedServices (service){
+        this.chosenServices.push(service);
+        alert(service + 'selected')
+    }
 }
