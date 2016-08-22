44b1e8366f13e3401616241043cfd45d9e77fad8
Quentin <quentin.lemarchand@hetic.net>
2016-08-22 11:57:52 +0200

    check duplicates value in lS

--- src/app/user-choice/user-choice.ts
+++ src/app/user-choice/user-choice.ts
@@ -1,17 +1,35 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'services',
-  styleUrls: [],
-  templateUrl: './user-choice.html'
+	selector: 'services',
+	styleUrls: [],
+	templateUrl: './user-choice.html'
 })
 
 export class UserChoice {
-    public chosenServices = [];
-    public servicesList = ['Flight', "Hotel", "Restaurant", "Car", "Delivery", "Interpreter", "VAT", "Hostess"];
-    
-    selectedServices (service){
-        this.chosenServices.push(service);
-        localStorage.menuItems = JSON.stringify(this.chosenServices);
-    }
+	public chosenServices = [];
+	public servicesList = ['Flight', "Hotel", "Restaurant", "Car", "Delivery", "Interpreter", "VAT", "Hostess"];
+
+	selectedServices(service) {
+		if (!localStorage.getItem("menuItems")) {
+			localStorage.setItem("menuItems", "[]");
+		}
+		var list = JSON.parse(localStorage.getItem("menuItems"));
+		var exist = false;
+		for (var i = 0; i < list.length; i++) {
+			if (list[i] == service) {
+				exist = true;
+			}
+		}
+		if (!exist) {
+			list.push(service);
+		}
+		else {
+
+		}
+		localStorage.setItem("menuItems", JSON.stringify(list));
+		for (var i = 0; i < localStorage.length; i++) {
+			console.log(localStorage.getItem(localStorage.key(i)))
+		}
+	}
 }
